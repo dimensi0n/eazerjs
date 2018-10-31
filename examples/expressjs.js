0bc46ba9ce1c46df7b8c56715e5b921cf7093fc1
Erwan ROUSSEL <erwan.roussel51@gmail.com>
2018-11-01 00:54:27 +0100

    Add express example

--- examples/expressjs.js
+++ examples/expressjs.js
@@ -1,5 +1,57 @@
 const { Component, Group, Page } = require('../eazer')
+
 const express = require('express')
 
 const app = express()
 
+const Types = {
+
+  Paragraph: {
+    type: 'p'
+  }
+
+}
+
+const MyFirstComponent = {
+
+  Alisases: {
+
+    p: Types.Paragraph
+
+  },
+
+  Content: {
+
+    p: 'Component 1'
+
+  }
+}
+
+const MySecondComponent = {
+
+  Alisases: {
+
+    p: Types.Paragraph
+
+  },
+
+  Content: {
+
+    p: 'Component 2'
+
+  }
+}
+
+app.get('/', (req, res) => {
+  const component1 = new Component('component1', MyFirstComponent.Content, MyFirstComponent.Alisases)
+  const component2 = new Component('component2', MySecondComponent.Content, MySecondComponent.Alisases)
+  const container = new Group('container', 'container')
+  container.include([component1, component2])
+
+  const page = new Page('MySuperPage', 'A super page made with Eazer', 'utf-8')
+  page.require([container])
+
+  res.end(page.render())
+})
+
+app.listen(8001)
